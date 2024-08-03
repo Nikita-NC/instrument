95389345d1ab8a74ee085d2d03517f0831965287
Nikita-NC <nikitachaudhary354@gmail.com>
2024-08-03 18:55:31 +0545

    update

--- src/item-description/item-description.component.ts
+++ src/item-description/item-description.component.ts
@@ -2,16 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { data_list } from '../assets/data';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { ActivatedRoute, Route, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Location } from '@angular/common';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { CartService } from '../cart.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-item-description',
   standalone: true,
-  imports: [FontAwesomeModule, CommonModule,NavBarComponent,RouterModule],
+  imports: [FontAwesomeModule, CommonModule, NavBarComponent, RouterModule],
   templateUrl: './item-description.component.html',
   styleUrl: './item-description.component.css',
 })
@@ -20,20 +21,30 @@ export class ItemDescriptionComponent {
 
   item: any;
 
-  constructor(private router: ActivatedRoute, private location: Location, private cartService: CartService) {}
+  constructor(
+    private router: ActivatedRoute,
+    private location: Location,
+    private cartService: CartService,
+    private path: Router
+  ) {}
 
   ngOnInit(): void {
     const id = this.router.snapshot.paramMap.get('id');
     this.item = data_list.find((item) => item.id === id);
   }
 
-  goBack(): void{
-    this.location.back()
+  goBack(): void {
+    this.location.back();
   }
 
-  addToCart(): void{
-    this.cartService.addToCart(this.item)
+  addToCart(): void {
+    const user = localStorage.getItem('userCredentials');
+    if (user) {
+      this.cartService.addToCart(this.item);
+      this.path.navigate(['/cart']);
+    } else {
+      alert('login first');
+      this.path.navigate(['/login']);
+    }
   }
-  
-    
 }
